f92bc13a1f6418c9e895a6df65a391266f955564
Ramesh-anon <anonymous9jock3r@gmail.com>
2025-05-26 13:54:33 +0530

    Add Cloudinary support

--- project-root/server.js
+++ project-root/server.js
@@ -1,3 +1,11 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
